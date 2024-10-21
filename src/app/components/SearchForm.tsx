81cd09a3ac003b913df047aa6ce86d250125645d
luizmarques <luiz_alak@hotmail.com>
2024-10-21 18:23:11 -0300

    chore: ajusts

--- src/app/components/SearchForm.tsx
+++ src/app/components/SearchForm.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
 export const SearchForm = ( {
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -24,7 +24,10 @@ export default async function SearchResults({ searchParams }: ISearchProps) {
           <Header />
           <main className='relative mb-48 mt-20 h-screen pl-4 lg:pl-16 '>
             <h1 className='mb-4 text-2xl font-bold'>
-              Search results for: <span className='text-red-500'>{title}</span>
+              Search results for movie Title: <span className='text-red-500'>{title}</span>
+            </h1>
+            <h1 className='mb-4 text-2xl font-bold'>
+              Search results for movie Genre: <span className='text-red-500'>{genre}</span>
             </h1>
             <p className='text-xl'>No movies found</p>
           </main>
@@ -39,7 +42,10 @@ export default async function SearchResults({ searchParams }: ISearchProps) {
         <Header />
         <main className='relative mb-48 mt-20 h-screen pl-4 lg:pl-16 '>
           <h1 className='mb-4 text-2xl font-bold'>
-            Search results for: <span className='text-red-500'>{title}</span>
+            Search results for movie title: <span className='text-red-500'>{title}</span>
+          </h1>
+          <h1 className='mb-4 text-2xl font-bold'>
+            Search results for movie Genre: <span className='text-red-500'>{genre}</span>
           </h1>
           <div className='grid grid-cols-2 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-8 lg:gap-8'>
             {movies.map((movie, index) => (
