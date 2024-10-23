ceee1926d5f996673ed913cf0d26d908e4e5bd8e
luizmarques <luiz_alak@hotmail.com>
2024-10-23 02:08:28 -0300

    chore: fix errors in vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['m.media-amazon.com'],
-  },
-  env: {
-    API_URL: process.env.API_URL,
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
 };
 
